d7d39bca07dad3900b8867dcac27371e47ad9da0
SyahrilSaputra23 <syahrilsaputra2323@gmail.com>
2021-05-25 00:06:46 +0800

    Mmebuat main_activity.xml, item_note.xml pada layout, main_menu pada directory menu, dan membuat vector asset ic_add.xml pada drawable

--- app/src/main/java/com/learningas/notewithroom/ui/MainActivity.kt
+++ app/src/main/java/com/learningas/notewithroom/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.learningas.notewithroom
+package com.learningas.notewithroom.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.learningas.notewithroom.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
